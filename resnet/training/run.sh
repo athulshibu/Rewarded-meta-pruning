b099ac92b9bd69f2bf5013fa0b0d75e1f826a408
Athul Shibu <athulshibu@gmail.com>
2022-11-21 13:45:31 +0900

    Update run.sh

--- resnet/training/run.sh
+++ resnet/training/run.sh
@@ -1,6 +1,2 @@
 clear
-python3 train.py --data=/mnt/local0/imagenet_dataset | tee -a log/training.txt
-
-python train.py --data=/home/sathul/Python/ImageNet2012 | tee -a log/training.txt
-
-python train.py --data=/home/sathul/Python/ImageNet2012 --epochs=33
+python train.py --data=./ImageNet2012 | tee -a log/training.txt
