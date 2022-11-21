4aaba27575e00a877af4b2dee5dcb1fa7162fb3d
Athul Shibu <athulshibu@gmail.com>
2022-11-21 13:44:47 +0900

    Update run.sh

--- resnet/searching/run.sh
+++ resnet/searching/run.sh
@@ -2,12 +2,4 @@ clear
 datename=$(date +%Y%m%d-%H%M%S)
 train_log=('genetic_iters_20_'$datename)
 mkdir log
-python3 search.py --max_iters=20 --net_cache='../training/models/checkpoint.pth.tar' --data=/mnt/local0/imagenet_dataset | tee log/$train_log.search.log
-
-python search.py --max_iters=20 --net_cache='../training/models/checkpoint.pth.tar' --data=/home/sathul/Python/ImageNet2012 --load_dict=$false | tee search.txt
-
-python search_v2.py --max_iters=20 --net_cache='../training/models/checkpoint.pth.tar' --data=/home/sathul/Python/ImageNet2012
-
-python search_v2.py --max_iters=20 --net_cache='../training/models/checkpoint.pth.tar' --data=/home/sathul/Python/ImageNet2012 | tee search_V2R2.txt
-
-python search_v3.py --max_iters=20 --net_cache='../training/models/checkpoint.pth.tar' --data=/home/sathul/Python/ImageNet2012 | tee search_V3R1.txt
+python search.py --max_iters=20 --net_cache='./training/models/checkpoint.pth.tar' --data='./ImageNet2012' | tee search.txt
